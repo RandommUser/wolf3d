305023821342104a403a76ed3051e638df2fca54
Petrus Hakakoski <phakakos@c1r3p1.hive.fi>
2020-11-09 13:53:14 +0200

    Start of the project

---
+++ Makefile
@@ -0,0 +1,26 @@
+NAME=wold3d
+SRCS=main.c
+BIGO=$(SRCS:.c=.o)
+INC=-I libft 
+LIB=-L ./libft -lft 
+FLAGS=-Wall -Wextra -Werror
+FRAMEWORK=
+PREFRAMEWORK=
+
+
+all : $(NAME)
+
+$(NAME) :
+	make -C ./libft
+	gcc -c $(FLAGS) $(SRCS) $(INC)
+	gcc $(FLAGS) $(PREFRAMEWORK) -o $(NAME) $(BIGO) $(LIB) $(FRAMEWORK)
+
+clean :
+	make -C ./libft clean
+	rm -f $(BIGO)
+
+fclean : clean
+	make -C ./libft fclean
+	rm -f $(NAME)
+
+re: fclean all
---
+++ header.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phakakos <phakakos@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/09 13:16:57 by phakakos          #+#    #+#             */
+/*   Updated: 2020/11/09 13:18:22 by phakakos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_H
+# define HEADER_H
+# define PRECISION float
+
+# include "libft.h"
+# include <math.h>
+# include <stdlib.h>
+#	include <stdio.h>
+# include <pthread.h>
+
+
+
+#endif
