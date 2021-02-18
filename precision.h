d68f6052572d7b771686b63a5490019bc6fc7f30
Petrus Hakakoski <phakakos@student.hive.fi>
2021-02-18 16:30:03 +0200

    added back to work with norm

---
+++ precision.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   precision.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phakakos <phakakos@hive.student.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/18 16:07:48 by phakakos          #+#    #+#             */
+/*   Updated: 2021/02/18 16:07:49 by phakakos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRECISION_H
+# define PRECISION_H
+# define PRECISION
+
+/*
+** don't ask...
+*/
+
+typedef double	t_precision;
+#endif
